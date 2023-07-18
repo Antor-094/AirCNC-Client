9072c6f8de308f1e0eb6273df00b9daa97beffe4
Antor-094 <mdantor556677889@gmail.com>
2023-07-18 19:50:07 +0600

    NavBar section added

---
+++ src/Components/shared/Navbar/Avatar.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+import { useContext } from 'react';
+import avatar from '../../../assets/images/placeholder.jpg'
+import { AuthContext } from '../../../providers/AuthProvider';
+const Avatar = () => {
+    const {user} = useContext(AuthContext)
+    return (
+        <img className='rounded-full' src={user && user.photoURL ? user.photoURL : avatar } alt="profile" height='30' width='30' />
+    );
+};
+
+export default Avatar;
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,10 +1,11 @@
 
-import React from 'react';
+// import React from 'react';
+import NavBar from '../Components/shared/NavBar/NavBar';
 
 const Main = () => {
   return (
     <div>
-      <h1>Welcome TO AirCNC</h1>
+     <NavBar></NavBar>
     </div>
   );
 };
