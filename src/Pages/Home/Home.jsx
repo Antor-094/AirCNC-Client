d722f3aa7df70b89f2ca05a7ccd0814533caf905
Antor-094 <mdantor556677889@gmail.com>
2023-07-23 09:14:30 +0600

    Card , loader , button component added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 // import React from 'react';
 
 import Categories from "../../Components/Categories/Categories";
+import Rooms from "../../Components/Rooms/Rooms";
 
 
 
@@ -8,6 +9,7 @@ const Home = () => {
     return (
         <>
             <Categories></Categories>
+            <Rooms></Rooms>
         </>
     );
 };
