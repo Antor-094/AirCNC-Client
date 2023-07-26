bd992ac768ea9344c9146e72ff47c229f1cbeb8d
Antor-094 <mdantor556677889@gmail.com>
2023-07-26 21:35:29 +0600

    Room data Fetched by category

--- src/Components/Rooms/Rooms.jsx
+++ src/Components/Rooms/Rooms.jsx
@@ -4,9 +4,14 @@ import { useEffect, useState } from "react";
 import Container from "../shared/Container/Container";
 import Card from "./Card";
 import Loader from "../shared/Loader/Loader";
+import { useSearchParams } from "react-router-dom";
 
 const Rooms = () => {
   const [rooms, setRooms] = useState([]);
+  const [params, setParams] = useSearchParams()
+
+    const category = params.get('category')
+    console.log(category)
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -14,19 +19,27 @@ const Rooms = () => {
     fetch("rooms.json")
       .then((res) => res.json())
       .then((data) => {
-        setRooms(data);
+        // console.log(data)
+        if(category){
+          const filtered = data.filter(room=>room.category === category)
+          setRooms(filtered)
+        }else{
+          setRooms(data); 
+        }
+       
         setLoading(false)
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [category]);
   if(loading){
     return <Loader />
   }
   return (
     <Container>
       <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {rooms.map((room) => (
-          <Card></Card>
+        {rooms.map((room,index) => (
+          <Card key={index} room={room}></Card>
+          
         ))}
       </div>
     </Container>
