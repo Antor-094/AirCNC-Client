b9bbb53091a8dc545133dc0f9c749bc15e4a55cc
Antor-094 <mdantor556677889@gmail.com>
2023-07-31 07:22:43 +0600

    Conditionally reserve button access and modal for reservation added

--- src/Components/Rooms/Calender.jsx
+++ src/Components/Rooms/Calender.jsx
@@ -3,14 +3,17 @@ import { DateRange } from 'react-date-range'
 import 'react-date-range/dist/styles.css'
 import 'react-date-range/dist/theme/default.css'
 
-const Calender = () => {
+const Calender = ({value,handleSelect}) => {
   return (
     <DateRange
-      rangeColors={['#262626']}
-      date={new Date()}
+    ranges={[value]}
+    onChange={handleSelect}
+      rangeColors={['#F43F5E']}
+      date={value.startDate}
       direction='vertical'
       showDateDisplay={false}
-      minDate={new Date()}
+      minDate={value.startDate}
+      maxDate={value.endDate}
     />
   )
 }
--- src/Components/Rooms/RoomReservation.jsx
+++ src/Components/Rooms/RoomReservation.jsx
@@ -1,28 +1,74 @@
 // import React from 'react';
 
+import { useContext, useState } from "react";
 import Button from "../Button/Button";
 import Calender from "./Calender";
+import { AuthContext } from "../../providers/AuthProvider";
+import BookingModal from "../Modal/BookingModal";
+import { formatDistance } from "date-fns";
 
-const RoomReservation = () => {
+const RoomReservation = ({ roomData }) => {
+  const { user, role } = useContext(AuthContext);
+  const [ isOpen, setIsOpen ] = useState(false);
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+// console.log(isOpen)
+  const totalPrice =
+    parseFloat(
+      formatDistance(new Date(roomData.to), new Date(roomData.from)).split(
+        " "
+      )[0]
+    ) * roomData.price;
+
+  const [value, setValue] = useState({
+    startDate: new Date(roomData?.from),
+    endDate: new Date(roomData?.to),
+    key: "selection",
+  });
+  const [bookingInfo, setBookingInfo] = useState({
+    guest: { name: user.displayName, email: user.email, image: user.photoURL },
+    host: roomData.host.email,
+    location: roomData.location,
+    price: totalPrice,
+    to: value.endDate,
+    from: value.startDate,
+  });
+  const handleSelect = () => {
+    setValue({ ...value });
+  };
+  const modalHandler = () => {
+    console.log(bookingInfo);
+  };
   return (
     <div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden ">
       <div className="flex flex-row items-center gap-1 p-4">
-        <div className="text-2xl font-semibold">$ 200</div>
+        <div className="text-2xl font-semibold">$ {roomData.price}</div>
         <div className="font-light text-neutral-600">night</div>
       </div>
       <hr />
 
       <div className="flex justify-center">
-      <Calender />
+        <Calender value={value} handleSelect={handleSelect} />
       </div>
       <hr />
       <div className="p-4">
-        <Button label='Reserve'/>
+        <Button
+          onClick={() => setIsOpen(true)}
+          disabled={roomData.host.email === user.email}
+          label="Reserve"
+        />
       </div>
       <div className="p-4 flex flex-row items-center justify-between font-semibold text-lg">
         <div>total</div>
-        <div>$ 300</div>
+        <div>$ {totalPrice}</div>
       </div>
+      <BookingModal
+        closeModal={closeModal}
+        modalHandler={modalHandler}
+        bookingInfo={bookingInfo}
+        isOpen={isOpen}
+      />
     </div>
   );
 };
